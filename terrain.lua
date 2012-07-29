5ff4f7c7e86e53f8240a8c5dfa39e461e1bb836e
Daniel Klein <bildzeitung@gmail.com>
2012-07-28 22:43:19 -0400

    Basic rasterization done; still need bresenham.

--- terrain.lua
+++ terrain.lua
@@ -9,6 +9,12 @@
 
 function p(x,y)
 	local m = { __tostring = function(s) return s.x..','..s.y end }
+
+	-- copy constructor
+	if type(x) == 'table' and not y then
+		y = x.y ; x = x.x
+	end
+	
 	return setmetatable({x=x,y=y},m)
 end
 
@@ -64,8 +70,58 @@ end
 -- do the subdivision
 while points.length > 1 do
 	points = fracture(points)
-	for k,v in ipairs(points) do print(v) end
-	print '--'
+	--for k,v in ipairs(points) do print(v) end
+	--print '--'
 end
 
+--  get bounding box
+local minpt = p(points[1])
+local maxpt = p(points[1])
+for _,v in ipairs(points) do
+	if v.x < minpt.x then minpt.x = v.x end
+	if v.y < minpt.y then minpt.y = v.y end
+
+	if v.x > maxpt.x then maxpt.x = v.x end
+	if v.y > maxpt.y then maxpt.y = v.y end
+end
+
+print("Bounds",minpt,maxpt)
+
 -- raster out the result into the map
+local imin = p(1,1)
+local imax = p(32,32)
+local stx  = (imax.x-imin.x)/(maxpt.x-minpt.x)
+local sty  = (imax.y-imin.y)/(maxpt.y-minpt.y)
+
+print('Scaling: ',stx,sty)
+
+local map = {}
+
+-- init map
+for i=1,imax.y do
+	map[i] = {}
+	for j =1,imax.x do
+		map[i][j] = 0
+	end
+end
+
+function displaymap(m)
+	for i=1,#m do
+		local s = ''
+		for j =1,#m[i] do
+			if m[i][j] >0 then s = s..'*' else s=s..' ' end
+		end
+		print(s)
+	end
+end
+
+-- transform window (line segment) space into view (map/raster) space 
+for k,v in ipairs(points) do
+	local x = math.floor((v.x-minpt.x)*stx+imin.x)
+	local y = math.floor((v.y-minpt.y)*sty+imin.y)
+	--print(v,x,y)
+	map[y][x] = 1
+	points[k] = p(x,y)
+end
+
+displaymap(map)
